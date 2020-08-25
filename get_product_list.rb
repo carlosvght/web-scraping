8522a336812adb8561a2d2ecd58cb85c782f6ecd
Carlos <carlosvaghetti01@gmail.com>
2020-08-25 17:07:20 -0300

    up github webscraping

---
+++ get_product_list.rb
@@ -0,0 +1,32 @@
+require 'mechanize'
+
+agent = Mechanize.new
+
+page = agent.get('http://localhost:3000')
+
+login_form = page.form
+
+login_form.field_with(name: 'user[email]').value = 'example@example.com'
+login_form.field_with(name: 'user[password]').value = '123456'
+
+product_list_page = agent.submit(login_form)
+
+out_file = File.new('product_list.txt', 'w')
+out_file.puts 'Product List: '
+
+
+loop do
+  product_list_page.search('tbody tr').each do |p|
+    f = p.search('td')
+    line = "Title: #{f[0].text}, "
+    line += "Brand: #{f[1].text}, "
+    line += "Description: #{f[2].text}, "
+    line += "Price: #{f[3].text}"
+    out_file.puts line
+  end
+  
+  break unless product_list_page.link_with(text: 'Next ›')
+  product_list_page = product_list_page.link_with(text: 'Next ›').click
+end
+
+out_file.close
